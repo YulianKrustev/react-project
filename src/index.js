af6bd6de45c9bb6fbfed4c5b1970c515617afb79
Julian Krustev <juliankrustev2018@gmail.com>
2022-10-20 18:30:02 +0300

    added grid layout

--- src/index.js
+++ src/index.js
@@ -1,11 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const Image = () => {
+  return (
+    <img
+      src="https://m.media-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg"
+      alt=""
+    />
+  );
+};
+
+const Title = () => {
+  return <h1>I Love You to the Moon and Back</h1>;
+};
+
+const Autohor = () => {
+  return <h4>Amelia Hepworth</h4>;
+};
+
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Autohor />
+    </article>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <h1>HI</h1>
+    <section className="bookList">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
   </React.StrictMode>
 );
-
-
