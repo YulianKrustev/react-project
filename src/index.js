2b5b7a68ef662afacaf0bad6a103d69f4c3c0d8d
Julian Krustev <juliankrustev2018@gmail.com>
2022-10-21 10:40:45 +0300

    Created first commponent Book

--- src/index.js
+++ src/index.js
@@ -1,45 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Book from "./Commponents/Book";
+
+// CSS
 import "./index.css";
 
-const Image = () => {
-  return (
-    <img
-      src="https://m.media-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg"
-      alt=""
-    />
-  );
-};
-
-const Title = () => {
-  return <h1>I Love You to the Moon and Back</h1>;
-};
-
-const Autohor = () => {
-  return <h4>Amelia Hepworth</h4>;
-};
-
-const Book = () => {
-  return (
-    <article className="book">
-      <Image />
-      <Title />
-      <Autohor />
-    </article>
-  );
+// Setup vars
+const firstBook = {
+  img: "https://m.media-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+  title: "I Love You to the Moon and Back",
+  author: "Amelia Hepworth",
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <section className="bookList">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
     </section>
   </React.StrictMode>
 );
